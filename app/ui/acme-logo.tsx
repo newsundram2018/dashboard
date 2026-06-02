6a11a4f46d40a4290a6a9ab65532dfac6635f16d
newsundram2018 <newsundram2018@gmail.com>
2026-06-02 09:05:23 +0530

    fonts fixed

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -4,7 +4,7 @@ import { roboto } from "@/app/ui/fonts";
 export default function AcmeLogo() {
   return (
     <div
-      className={`${roboto.className} flex flex-row items-center leading-none text-white`}
+      className={`${roboto.className} flex flex-row items-end backdrop-red-200 leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
       <p className="text-[44px]">Acme</p>
